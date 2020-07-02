f15af72d3d85833853efebc79058c4527d4d505e
Walt Deyzel <waltdeyzel3141@gmail.com>
2020-07-02 17:43:50 +0200

    Time stamp time cards date and time display with minor theming.

--- lib/screens/time_stamps_screen.dart
+++ lib/screens/time_stamps_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../models/time_stamps.dart';
 import '../models/project.dart';
 import '../screens/timer_screen.dart';
@@ -8,8 +9,7 @@ class TimeStampsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final project =
-        ModalRoute.of(context).settings.arguments as Project;
+    final project = ModalRoute.of(context).settings.arguments as Project;
     final List<TimeStamps> timeStamps = project.getTimeStamps();
     return Scaffold(
       appBar: AppBar(
@@ -20,18 +20,22 @@ class TimeStampsScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: timeStamps.length,
         itemBuilder: (context, index) {
-          return Container(
-            margin: EdgeInsets.only(bottom: 10),
-            height: 100,
+          return Card(
             color: Colors.pink,
             child: Column(children: <Widget>[
-              Text(
-                timeStamps[index]
-                        .endTime
-                        .difference(timeStamps[index].startTime)
-                        .inMinutes
-                        .toString() +
-                    " min",
+              Container(
+                height: 25,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                        margin: EdgeInsets.only(left: 5),
+                        child: date(timeStamps[index])),
+                    Container(
+                        margin: EdgeInsets.only(right: 5),
+                        child: duration(timeStamps[index])),
+                  ],
+                ),
               ),
               Divider(),
               Text(timeStamps[index].note),
@@ -39,9 +43,39 @@ class TimeStampsScreen extends StatelessWidget {
           );
         },
       ),
-      floatingActionButton: FloatingActionButton(child: Icon(Icons.access_alarm), onPressed: () {
-        Navigator.of(context).pushReplacementNamed(TimerScreen.routeName, arguments: project);
-      },),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.access_alarm),
+        onPressed: () {
+          Navigator.of(context)
+              .pushReplacementNamed(TimerScreen.routeName, arguments: project);
+        },
+      ),
     );
   }
+}
+
+Widget duration(TimeStamps timeStamps) {
+  Duration time = timeStamps.endTime.difference(timeStamps.startTime);
+  if (time.inHours > 0)
+    return Text(
+      time.inHours.toString() +
+          "h " +
+          (time.inMinutes % 60).toString() +
+          "min " +
+          (time.inSeconds % 60).toString() +
+          "s",
+    );
+  return Text(
+    ((time.inMinutes % 60).toString().padLeft(1, "0") +
+        "min " +
+        (time.inSeconds % 60).toString().padLeft(2, "0") +
+        "s"),
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+  );
+}
+
+Widget date(TimeStamps timeStamps) {
+  return Text(
+      DateFormat(DateFormat.ABBR_MONTH_WEEKDAY_DAY).format(timeStamps.endTime),
+      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),);
 }
