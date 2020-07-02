64423f83ead0eaecda24aa7a32cf8e1e43037c8d
Walt Deyzel <waltdeyzel3141@gmail.com>
2020-07-02 11:26:20 +0200

    Basic theming for timer screen.

--- lib/screens/timer_screen.dart
+++ lib/screens/timer_screen.dart
@@ -5,7 +5,23 @@ class TimerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Center(child: Text("00:00:00", style: TextStyle(fontSize: 40),), ),
+            color: Colors.amber,
+            height: 400,
+          ),
+          Container(
+            height: 283,
+            color: Colors.amber,
+            child: FlatButton(
+              onPressed: null,
+              child: Center(child: Icon(Icons.play_arrow, size: 150.0,),),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
