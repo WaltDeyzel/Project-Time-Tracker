5146ea3cbb513e8342a40d304c04b889835e2ca0
Walt Deyzel <waltdeyzel3141@gmail.com>
2020-07-02 11:59:23 +0200

    Stopwatch implemented.

--- lib/screens/timer_screen.dart
+++ lib/screens/timer_screen.dart
@@ -1,14 +1,57 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class TimerScreen extends StatelessWidget {
+class TimerScreen extends StatefulWidget {
   static const routeName = '/timer';
+
+  @override
+  _TimerScreenState createState() => _TimerScreenState();
+}
+
+class _TimerScreenState extends State<TimerScreen> {
+  final Duration _duration = const Duration(seconds: 1);
+  bool _startOrPause = false;
+  String _timeDisplay = "00:00:00";
+
+  var _stopwatch = Stopwatch();
+
+  void _startTimer(){
+    Timer(_duration, _keepRunning);
+  }
+
+  void _keepRunning(){
+    if(_stopwatch.isRunning){
+      _startTimer();
+    }
+    setState(() {
+      _timeDisplay = _stopwatch.elapsed.inHours.toString().padLeft(2, "0") + ":" +
+      (_stopwatch.elapsed.inMinutes%60).toString().padLeft(2, "0") + ":" +
+      (_stopwatch.elapsed.inSeconds%60).toString().padLeft(2, "0");
+    });
+  }
+  void _startStopwatch(){
+    setState(() {
+      _startOrPause = false;
+    });
+    _stopwatch.start();
+    _startTimer();
+  }
+  void _stopStopWatch(){
+    setState(() {
+      _startOrPause = true;
+    });
+    _stopwatch.stop();
+    _stopwatch.reset();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
           Container(
-            child: Center(child: Text("00:00:00", style: TextStyle(fontSize: 40),), ),
+            child: Center(child: Text(_timeDisplay, style: TextStyle(fontSize: 40),), ),
             color: Colors.amber,
             height: 400,
           ),
@@ -16,8 +59,11 @@ class TimerScreen extends StatelessWidget {
             height: 283,
             color: Colors.amber,
             child: FlatButton(
-              onPressed: null,
-              child: Center(child: Icon(Icons.play_arrow, size: 150.0,),),
+              onPressed: (){
+                if(_startOrPause)_startStopwatch();
+                else _stopStopWatch();
+              },
+              child: Center(child: _startOrPause ? Icon(Icons.play_arrow, size: 150.0,) : Icon(Icons.pause, size: 150.0,) ,),
             ),
           ),
         ],
