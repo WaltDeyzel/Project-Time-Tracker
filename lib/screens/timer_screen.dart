74244c1be55cac1216445c1bc7ef867f9b7ec571
Walt Deyzel <waltdeyzel3141@gmail.com>
2020-07-02 18:31:26 +0200

    Can now add notes during timed session.

--- lib/screens/timer_screen.dart
+++ lib/screens/timer_screen.dart
@@ -18,18 +18,20 @@ enum ButtonTimer {
 
 class _TimerScreenState extends State<TimerScreen> {
   final Duration _duration = const Duration(seconds: 1);
+  final _form = GlobalKey<FormState>();
   DateTime _startTime;
   DateTime _endTime;
   ButtonTimer _startOrPause = ButtonTimer.Play;
   String _timeDisplay = "00:00:00";
+  String _note;
 
   var _stopwatch = Stopwatch();
 
-  void _startTimer(){
+  void _startTimer() {
     Timer(_duration, _keepRunning);
   }
 
-  void _keepRunning(){
+  void _keepRunning() {
     if (_stopwatch.isRunning) {
       _startTimer();
     }
@@ -63,64 +65,81 @@ class _TimerScreenState extends State<TimerScreen> {
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
-    final project =
-        ModalRoute.of(context).settings.arguments as Project;
+    final project = ModalRoute.of(context).settings.arguments as Project;
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          GestureDetector(
-            onLongPress: () {
-              _stopwatch.reset();
-              _startOrPause = ButtonTimer.Play;
-              Navigator.of(context).pop();
-            },
-            child: Container(
-              child: Center(
-                child: Text(
-                  _timeDisplay,
-                  style: TextStyle(fontSize: 40),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              onLongPress: () {
+                _stopwatch.reset();
+                _stopwatch.stop();
+                _startOrPause = ButtonTimer.Play;
+              },
+              child: Container(
+                child: Center(
+                  child: Text(
+                    _timeDisplay,
+                    style: TextStyle(fontSize: 40),
+                  ),
+                ),
+                color: Colors.pink,
+                height: 1 * height / 3,
+              ),
+            ),
+            Container(
+              height: 1 * height / 3,
+              color: Colors.pink,
+              child: Form(
+                key: _form,
+                child: TextFormField(
+                  decoration: InputDecoration(labelText: "Note"),
+                  maxLines: 5,
+                  onSaved: (newValue) {
+                    _note = newValue;
+                  },
                 ),
               ),
+            ),
+            Container(
+              height: height / 3,
               color: Colors.pink,
-              height: 2 * height / 3,
-            ),
-          ),
-          Container(
-            height: height / 3,
-            color: Colors.pink,
-            child: FlatButton(
-              onLongPress: () {
-                if(_timeDisplay != "00:00:00")
-                project.addTimeStamp(TimeStamps(id: "AA1", startTime: _startTime, endTime: _endTime, note: "New note"));
-                Navigator.of(context).pop();
-              },
-              onPressed: () {
-                if (_startOrPause == ButtonTimer.Play) {
-                  _startTime = DateTime.now();
-                  _startStopwatch();
-                  
-                }
-                else if (_startOrPause == ButtonTimer.Pause) {
-                  _endTime = DateTime.now();
-                  _stopStopwatch();
-                }
-              },
-              child: _startOrPause == ButtonTimer.Play
-                  ? Center(
-                      child: Icon(
-                        Icons.play_arrow,
-                        size: 150,
+              child: FlatButton(
+                onLongPress: () {
+                  if (_timeDisplay != "00:00:00") _form.currentState.save();
+                  project.addTimeStamp(TimeStamps(
+                      id: "AA1",
+                      startTime: _startTime,
+                      endTime: _endTime,
+                      note: _note.length == 0 ? "..." : _note));
+                  Navigator.of(context).pop();
+                },
+                onPressed: () {
+                  if (_startOrPause == ButtonTimer.Play) {
+                    _startTime = DateTime.now();
+                    _startStopwatch();
+                  } else if (_startOrPause == ButtonTimer.Pause) {
+                    _endTime = DateTime.now();
+                    _stopStopwatch();
+                  }
+                },
+                child: _startOrPause == ButtonTimer.Play
+                    ? Center(
+                        child: Icon(
+                          Icons.play_arrow,
+                          size: 150,
+                        ),
+                      )
+                    : Center(
+                        child: Icon(
+                          Icons.pause,
+                          size: 150,
+                        ),
                       ),
-                    )
-                  : Center(
-                      child: Icon(
-                        Icons.pause,
-                        size: 150,
-                      ),
-                    ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
